95634bc484303df41ce6943663e8d2d6469b872b
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-10-26 15:24:08 -0400

    add DayListItem to storybook and do component isolation test

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "components/Application.scss";
 import Button from "./Button";
-
+import DayListItem from './DayListItem';
 export default function Application(props) {
   return (
     <main className="layout">
