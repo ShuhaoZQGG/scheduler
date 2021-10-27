932d283ea8ec16419e4acd6b6eab62f4cbebe886
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-10-27 00:03:06 -0400

    create component isolation and test for InterviewerListtItem and InterviewerList using storybook

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import "./InterviewerList.scss";
+
+export default function InterviewerList(props) {
+  const {interviewers, setInterviewer, interviewer} = props;
+  const InterviewerListItems = interviewers.map((eachInterviewer) => 
+      <InterviewerListItem 
+        id = {eachInterviewer.id}
+        name = {eachInterviewer.name}
+        avatar = {eachInterviewer.avatar}
+        selected = {eachInterviewer.id === interviewer}
+        setInterviewer = {setInterviewer}
+      />
+  )
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {InterviewerListItems}
+      </ul>
+    </section>
+  )
+}
