5b35f1f6499847a9cb5782caff3caed32bbb6187
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-10-27 20:35:02 -0400

    import Appointment to Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,29 +1,79 @@
 import React, {useState} from "react";
 
 import "components/Application.scss";
-// import Button from "./Button";
+import Button from "./Button";
 import DayList from './DayList';
+import Appointment from "./Appointment";
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+
 export default function Application(props) {
   
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
-
   const [date, setDate] = useState("Monday");
+
+  const appointmentListItems = appointments.map((appointment) => 
+    <Appointment 
+        key={appointment.id}
+        id={appointment.id}
+        time = {appointment.time}
+        interview={appointment.interview}
+      />
+  )
 
   return (
     <main className="layout">
@@ -50,6 +100,7 @@ export default function Application(props) {
       <section className="schedule">
         {/* <Button confirm = "hello" className = "button" onClick = {alert("hi")} dsiabled = "disabled"> Confirm</Button> */}
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentListItems}
       </section>
     </main>
   );
