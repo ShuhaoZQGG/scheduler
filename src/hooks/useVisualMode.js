e674a14a20c325b4d53925fe05cb879bc81528ad
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-10-30 11:29:39 -0400

    implement custom hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,16 +4,26 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(next) { 
-    setMode(next);
-    history.push(next)
-    setHistory(history)
-    console.log(history);
-
+  function transition(next, replace = false) { 
+    if (replace === true) {
+      back();
+      setMode(next);
+      history.push(next);
+      setHistory(history) ;
+    } else {
+      setMode(next);
+      history.push(next) 
+      setHistory(history)
+    }
   }
+
   function back() { 
+    if (history.length <= 1) {
+      setMode(history[0])
+    } else {
     history.pop(); 
     setMode(history[history.length-1]);
+    }
   }
 
   return { mode, transition, back };
