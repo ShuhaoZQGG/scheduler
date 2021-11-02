49eeeba357f3a66d60345376d5aa0414c0397415
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-11-02 18:59:59 -0400

    complete both edit and delete Interview Cypress end-to-end test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -20,4 +20,34 @@ describe("Appointments", () => {
    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
    cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
+  
+  it("should edit an interview", () => {
+    cy.get('[alt="Edit"]').first().click({ force: true });
+    cy.get('[alt="Tori Malcolm"]').click();
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  it("should delete an interview", () => {
+    cy.get('[alt="Delete"]').first().click({ force: true });
+    cy.contains("Cancel").click();
+
+    cy.contains(".appointment__card--show", "Archie Cohen");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+
+    cy.get('[alt="Delete"]').first().click({ force: true });
+    cy.contains("Confirm").click();
+
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
+    cy.contains(".appointment__card--show", "Sylvia Palmer")
+      .should("not.exist");
+  })
  });
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,12 +11,14 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+const initCypressMousePositionPlugin = require('cypress-mouse-position/plugin');
 
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
+  initCypressMousePositionPlugin(on);
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
